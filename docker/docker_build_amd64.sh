57aa8083c8236819f4241ddeef57ddfa6e4c2be7
Beniamino Pozzan <beniamino.pozzan@gmail.com>
2025-09-01 21:27:36 +0100

    chore: replace docker tag with `dronecode/roscon-25-workshop`
    Signed-off-by: Beniamino Pozzan <beniamino.pozzan@gmail.com>

--- docker/docker_build_amd64.sh
+++ docker/docker_build_amd64.sh
@@ -1,4 +1,4 @@
 SCRIPT=$(realpath "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 
-docker build -t px4/roscon-25-workshop -f ${SCRIPTPATH}/Dockerfile  ${SCRIPTPATH}/..
+docker build -t dronecode/roscon-25-workshop -f ${SCRIPTPATH}/Dockerfile  ${SCRIPTPATH}/..
