25959a7890fe3102a0d72ab999a9cb0881d2bf6c
Beniamino Pozzan <beniamino.pozzan@gmail.com>
2025-10-05 16:38:28 +0100

    chore: do not install QGC for arm64 targets
    Signed-off-by: Beniamino Pozzan <beniamino.pozzan@gmail.com>

--- docker/scripts/install_qgc.sh
+++ docker/scripts/install_qgc.sh
@@ -3,10 +3,15 @@ set -eo pipefail
 
 QGC_VERSION=$1
 
-cd /home/${USER}
-wget https://github.com/mavlink/qgroundcontrol/releases/download/${QGC_VERSION}/QGroundControl-x86_64.AppImage
-chmod +x ./QGroundControl-x86_64.AppImage
-./QGroundControl-x86_64.AppImage --appimage-extract
-mv /home/${USER}/squashfs-root /home/${USER}/QGroundControl
-chmod +x /home/${USER}/QGroundControl/AppRun
-ln -s /home/${USER}/QGroundControl/AppRun /home/${USER}/QGroundControl/qgroundcontrol
+if ["${TARGETARCH}" = "amd64" ]; then
+    cd /home/${USER}
+    wget https://github.com/mavlink/qgroundcontrol/releases/download/${QGC_VERSION}/QGroundControl-x86_64.AppImage
+    chmod +x ./QGroundControl-x86_64.AppImage
+    ./QGroundControl-x86_64.AppImage --appimage-extract
+    mv /home/${USER}/squashfs-root /home/${USER}/QGroundControl
+    chmod +x /home/${USER}/QGroundControl/AppRun
+    ln -s /home/${USER}/QGroundControl/AppRun /home/${USER}/QGroundControl/qgroundcontrol
+else
+    mkdir -p /home/${USER}/QGroundControl
+    echo "QGroundControl is only available for amd64 architecture." >> /home/${USER}/QGroundControl/install.log
+fi
