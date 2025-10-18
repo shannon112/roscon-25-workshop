9f517303fb99ffb7d6b0a603154b3c053a7558f8
Beniamino Pozzan <beniamino.pozzan@gmail.com>
2025-10-18 09:41:41 +0100

    feat: update teleop to use common launchfile (#25)
    Signed-off-by: Beniamino Pozzan <beniamino.pozzan@gmail.com>

--- px4_roscon_25/teleop/README.md
+++ px4_roscon_25/teleop/README.md
@@ -1,14 +1,44 @@
 # teleop
 
-This packages offers uses PX4 custom modes to teleop a drone.
+This packages offers uses PX4 custom modes and executor to teleop a drone.
 
 ## Usage
 
-1. Start the simulation, PX4 and QGC as described in the [docker guide](../../docker/README.md).
-2. Run `teleop.launch.py` from inside the docker container
+Launch gz [walls](https://github.com/PX4/PX4-gazebo-models/blob/e05f4312d3f28aa621157610584a4870406cb6d3/worlds/walls.sdf) world, you'll have to navigate inside narrow passages now.
 
-   ```sh
-   docker exec -it px4-roscon-25 bash -ic "ros2 launch teleop teleop.launch.py run_uxrcedds_agent:=true"
-   ```
+First start the container, then run
 
-The `teleop.launch.py` can also start the _MicroXrceAgent_. Set the launch argument `run_uxrcedds_agent` to `true` to run it.
+```sh
+python3 /home/ubuntu/PX4-gazebo-models/simulation-gazebo --model_store /home/ubuntu/PX4-gazebo-models/ --world walls
+```
+
+Spawn model and attach PX4 SITL
+
+```sh
+PX4_GZ_STANDALONE=1 PX4_SYS_AUTOSTART=4013 PX4_PARAM_UXRCE_DDS_SYNCT=0 /home/ubuntu/px4_sitl/bin/px4 -w /home/ubuntu/px4_sitl/romfs
+```
+
+Here we used `PX4_SYS_AUTOSTART=4013` which set the Gazebo model to _x500_lidar_2d_.
+Don't forget to start QGC too.
+
+When the simulation is running you can see the GZ topics:
+
+```sh
+gz topic -l
+```
+
+You can now launch the [common](../px4_roscon_25/README.md) launchfile
+
+```sh
+ros2 launch px4_roscon_25 common.launch.py
+```
+
+and finally the `teleop` launchfile and the _keyboard_monitor_ node.
+
+```sh
+ros2 launch telop teleop.launch.py
+```
+
+```sh
+ros2 run teleop_twist_rpyt_keyboard teleop_twist_rpyt_keyboard
+```
--- px4_roscon_25/teleop/launch/teleop.launch.py
+++ px4_roscon_25/teleop/launch/teleop.launch.py
@@ -1,33 +1,23 @@
 import os
 
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, ExecuteProcess
-from launch.conditions import IfCondition
-from launch.substitutions import LaunchConfiguration
-from launch_ros.actions import Node
-from ament_index_python.packages import get_package_share_directory
+from launch_ros.actions import LoadComposableNodes, Node
+from launch_ros.substitutions import FindPackageShare
+from launch_ros.descriptions import ComposableNode
 
 def generate_launch_description():
 
-    pkg_share = get_package_share_directory("teleop")
+    pkg_share = FindPackageShare("teleop").find("teleop")
 
-    clock_bridge_config_file = os.path.join(pkg_share,"cfg","clock_bridge.yaml")
-    teleop_config_file = os.path.join(pkg_share, 'cfg', 'params.yaml')
-
-    run_uxrcedds_agent_arg = DeclareLaunchArgument(
-        "run_uxrcedds_agent",
-        default_value="false",
-        description="Whether to run the MicroXRCEdds Agent",
-    )
+    bridge_config_file = os.path.join(pkg_share, "cfg", 'bridge.yaml')
 
     return LaunchDescription([
-        run_uxrcedds_agent_arg,
         Node(
             package="ros_gz_bridge",
             executable="parameter_bridge",
             name="gz_clock_bridge",
             parameters=[
-                {"config_file": clock_bridge_config_file}
+                {"config_file": bridge_config_file}
             ]
         ),
         Node(
@@ -39,9 +29,26 @@ def generate_launch_description():
                 {"use_sim_time": True}
             ]
         ),
-        ExecuteProcess(
-            cmd=["MicroXRCEAgent", "udp4", "-p", "8888", "-v", "3"],
-            output="screen",
-            condition=IfCondition(LaunchConfiguration("run_uxrcedds_agent"))
-        )
+        LoadComposableNodes(
+            target_container='static_tf_container',
+            composable_node_descriptions=[
+                ComposableNode(
+                    package='tf2_ros',
+                    plugin='tf2_ros::StaticTransformBroadcasterNode',
+                    name='map_to_odom_broadcaster',
+                    parameters=[{
+                        'use_sim_time': True,
+                        'translation.x': 0.0,
+                        'translation.y': 0.0,
+                        'translation.z': 0.0,
+                        'rotation.x': 0.0,
+                        'rotation.y': 0.0,
+                        'rotation.z': 0.0,
+                        'rotation.w': 1.0,
+                        'frame_id': 'base_link',
+                        'child_frame_id': 'x500_lidar_2d_0/link/lidar_2d_v2'
+                    }]
+                ),
+            ]
+        ),
     ])
