04762a51ba4edbf93dae43c108002819023897c1
Patrik Dominik Pordi <119634185+patrikpordi@users.noreply.github.com>
2025-10-22 17:35:34 -0400

    fix: Update search waypoints to use vehicle position (#36)

--- px4_roscon_25/precision_land_executor/CustomMode_v2.cpp
+++ px4_roscon_25/precision_land_executor/CustomMode_v2.cpp
@@ -305,12 +305,12 @@ void PrecisionLand::generateSearchWaypoints()
 	// Generate spiral search waypoints
 	// The search waypoints are generated in the NED frame
 	// Parameters for the search pattern
-	double start_x = 0.0;
-	double start_y = 0.0;
+	double start_x = _vehicle_local_position->positionNed().x();
+	double start_y = _vehicle_local_position->positionNed().y();
 	double current_z = _vehicle_local_position->positionNed().z();
 	auto min_z = -1.0;
 
-	double max_radius = 2.0;
+	double max_radius = 1.0;
 	double layer_spacing = 0.5;
 	int points_per_layer = 16;
 	std::vector<Eigen::Vector3f> waypoints;
